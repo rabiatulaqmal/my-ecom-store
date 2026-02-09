bb26d945e95f163583154fa4f003cd51468f67df
rabiatulaqmal <rabiatulaqma99@gmail.com>
2026-02-10 03:11:43 +0800

    Installed Pillow for product image support

--- shop/models.py
+++ shop/models.py
@@ -1,10 +1,10 @@
-from django.db import models
+from django.db import db
 
-class Product(models.Model):
-    name = models.CharField(max_length=200)
-    description = models.TextField()
-    price = models.DecimalField(max_digits=10, decimal_places=2)
-    stock = models.IntegerField()
+class Product(db.Model):
+    name = db.CharField(max_length=200)
+    description = db.TextField()
+    price = db.DecimalField(max_digits=10, decimal_places=2)
+    image = db.ImageField(upload_to='products/', null=True, blank=True) # New Field
 
     def __str__(self):
         return self.name
--- shop/views.py
+++ shop/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render
+from django.http import JsonResponse
+from .models import Product
 
-# Create your views here.
 def home(request):
     return render(request, 'shop/home.html')
+
+def product_list_api(request):
+    # This fetches all products from your database for Rabiatul Aqmal Store
+    products = Product.objects.all().values('id', 'name', 'price', 'description')
+    return JsonResponse(list(products), safe=False)
