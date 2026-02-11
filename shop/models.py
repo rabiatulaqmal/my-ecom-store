1e94ed4075c4863d92c67ccec3642364eb117ea9
rabiatulaqmal <rabiatulaqma99@gmail.com>
2026-02-11 22:44:24 +0800

    Fixed model import error and enabled product images

--- shop/models.py
+++ shop/models.py
@@ -1,10 +1,10 @@
-from django.db import db
+from django.db import models # Use 'models', not 'db'
 
-class Product(db.Model):
-    name = db.CharField(max_length=200)
-    description = db.TextField()
-    price = db.DecimalField(max_digits=10, decimal_places=2)
-    image = db.ImageField(upload_to='products/', null=True, blank=True) # New Field
+class Product(models.Model): # Use 'models.Model'
+    name = models.CharField(max_length=200)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    image = models.ImageField(upload_to='products/', null=True, blank=True)
 
     def __str__(self):
         return self.name
